8109bebe219dcfac10a17e1eda04475f68951551
wsxqead <lsj122993@gmail.com>
2025-02-04 01:54:35 +0900

    ㅌㅅㅌㅌ

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
 export default function Home() {
   return (
-    <div className="text-center">
-      <h1 className="text-4xl font-bold">
+    <main className="flex-1 container mx-auto p-6">
+      <h1 className="text-4xl font-bold text-center">
         🎉 유즈하 리코 1주년 기념 사이트 🎉
       </h1>
-      <p className="mt-4 text-lg">
+      <p className="text-lg text-center text-gray-300 mt-2">
         리코의 1년을 함께 돌아보고, 팬들과 즐기는 공간!
       </p>
-    </div>
+    </main>
   );
 }
