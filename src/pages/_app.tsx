69acadaa4a7a4167284a14fd55b7c7426ad90a2c
wsxqead <lsj122993@gmail.com>
2025-02-05 18:02:35 +0900

    사이트 헤더 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,24 @@ import "../styles/globals.css";
 
 import type { AppProps } from "next/app";
 import Layout from "../components/layout"; // 공통 레이아웃 추가
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <title>유즈하 리코 1주년 기념 사이트</title>
+        <meta name="description" content="리코의 1년을 기념하는 팬 사이트" />
+        <meta property="og:title" content="유즈하 리코 1주년 기념 사이트" />
+        <meta
+          property="og:description"
+          content="리코의 1년을 함께 돌아보는 공간"
+        />
+        <meta property="og:image" content="/images/main.png" />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
